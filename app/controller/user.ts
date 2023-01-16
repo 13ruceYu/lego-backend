e4763f572fa276ffa7d4e19167a47b4297ef6ad2
13ruceYu <bruceyuhongbo@gmail.com>
2023-01-16 22:42:27 +0800

    feat: use egg-validate

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -1,8 +1,22 @@
 import { Controller } from 'egg';
 
+const userCreateRules = {
+  username: 'email',
+  password: {
+    type: 'password',
+    min: 8,
+  },
+};
+
 export default class UserController extends Controller {
-  async createByEmail(ctx) {
-    const { service } = this;
+  async createByEmail() {
+    const { service, ctx, app } = this;
+    // ctx.validate(userCreateRules);
+    const errors = app.validator.validate(userCreateRules, ctx.request.body);
+    ctx.logger.warn(errors);
+    if (errors) {
+      return ctx.helper.error({ ctx, errno: 10001, msg: '验证码错误' });
+    }
     const userData = await service.user.createByEmail(ctx.request.body);
     ctx.helper.success({ ctx, res: userData });
   }
--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -6,12 +6,25 @@ interface RespType {
   msg?: string;
 }
 
+interface ErrorRespType {
+  ctx: Context;
+  errno: number;
+  msg?: string;
+}
+
 export default {
   success({ ctx, res, msg }: RespType) {
     ctx.body = {
       errno: 0,
       data: res ? res : null,
       message: msg ? msg : 'success',
+    };
+    ctx.status = 200;
+  },
+  error({ ctx, msg, errno }: ErrorRespType) {
+    ctx.body = {
+      errno,
+      message: msg ? msg : '请求错误',
     };
     ctx.status = 200;
   },
