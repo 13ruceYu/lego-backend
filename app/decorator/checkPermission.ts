d4c30566e336d667f2ba4e50f34fadae05d066fa
13ruceYu <bruceyuhongbo@gmail.com>
2023-02-06 06:09:21 +0800

    feat: RBAC with casl

--- app/decorator/checkPermission.ts
+++ app/decorator/checkPermission.ts
@@ -1,7 +1,16 @@
+import { subject } from '@casl/ability';
 import { Controller } from 'egg';
 import { GlobalErrorTypes } from '../error';
+import defineRoles from '../roles/roles';
 
-export default function checkPermission(modelName: string, errorType: GlobalErrorTypes, userKey = 'user') {
+const caslMethodMapping: Record<string, string> = {
+  GET: 'read',
+  POST: 'create',
+  PATCH: 'update',
+  DELETE: 'delete',
+};
+
+export default function checkPermission(modelName: string, errorType: GlobalErrorTypes) {
   return function(_prototype, _key: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     descriptor.value = async function(...args: any[]) {
@@ -10,9 +19,25 @@ export default function checkPermission(modelName: string, errorType: GlobalErro
       // @ts-ignore
       const { ctx } = that;
       const { id } = ctx.params;
-      const userId = ctx.state.user._id;
-      const certainRecord = await ctx.model[modelName].findOne({ id });
-      if (!certainRecord || certainRecord[userKey].toString() !== userId) {
+      const { method } = ctx.request;
+      const action = caslMethodMapping[method];
+      if (!ctx.state && !ctx.state.user) {
+        return ctx.helper.error({ ctx, errorType });
+      }
+      let permission = false;
+      // 获取定义的 roles
+      const ability = defineRoles(ctx.state.user);
+      // 获取 rule 来判断是否存在对应的条件
+      const rule = ability.relevantRuleFor(action, modelName);
+      if (rule && rule.conditions) {
+        const certainRecord = await ctx.model[modelName].findOne({ id }).lean();
+        permission = ability.can(action, subject(modelName, certainRecord));
+      } else {
+        permission = ability.can(action, modelName);
+      }
+      // const userId = ctx.state.user._id;
+      // const certainRecord = await ctx.model[modelName].findOne({ id });
+      if (!permission) {
         return ctx.helper.error({ ctx, errorType });
       }
       await originalMethod.apply(this, args);
---
+++ app/roles/roles.ts
@@ -0,0 +1,30 @@
+import { createMongoAbility, AbilityBuilder } from '@casl/ability';
+import { Document } from 'mongoose';
+import { UserProps } from '../model/user';
+
+export default function defineRoles(user: UserProps & Document<any, any, UserProps>) {
+  const { can, build } = new AbilityBuilder(createMongoAbility);
+  if (user) {
+    if (user.role === 'admin') {
+      can('manage', 'all');
+    } else {
+      // normal login user
+      // users: 职能读取自己的信息，以及更新特殊字段
+      can('read', 'User', { _id: user._id });
+      can('update', 'User', [ 'nickname', 'picture' ], { _id: user._id });
+
+      // works: 可以创建，更新和删除自己的 work
+      can('create', 'Work', [ 'title', 'desc', 'content', 'coverImg' ]);
+      can('read', 'Work', { user: user._id });
+      can('update', 'Work', [ 'title', 'desc', 'content', 'coverImg' ], { user: user._id });
+      can('delete', 'Work', { user: user._id });
+
+      // channels: 创建更新删除自己的 channel
+      can('create', 'Channel', [ 'name', 'workId' ], { user: user._id });
+      can('read', 'Channel', { user: user._id });
+      can('update', 'Channel', [ 'name' ], { user: user._id });
+      can('delete', 'Channel', [ 'name' ], { user: user._id });
+    }
+  }
+  return build();
+}
