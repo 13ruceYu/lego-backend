dbba50a708113b8f0d731aebe0a70cdb10f30842
13ruceYu <bruceyuhongbo@gmail.com>
2023-09-21 07:04:32 +0800

    feat: use egg-validate validate request body

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -1,17 +1,30 @@
 import { Context } from 'egg'
 
-interface RespType {
+interface IRespType {
   ctx: Context;
   res?: any;
   msg?: string;
 }
 
+interface IErrorRespType {
+  ctx: Context;
+  errno: number;
+  msg?: string;
+}
+
 export default {
-  success({ ctx, res, msg }: RespType) {
+  success({ ctx, res, msg }: IRespType) {
     ctx.body = {
       errno: 0,
       data: res ? res : null,
       message: msg ? msg : '请求成功'
+    }
+    ctx.status = 200
+  },
+  error({ ctx, msg, errno }: IErrorRespType) {
+    ctx.body = {
+      errno,
+      message: msg ? msg : '请求错误'
     }
     ctx.status = 200
   }
