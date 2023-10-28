3b44433f4091365320e70ee4a0422d7a5c5c667c
13ruceYu <bruceyuhongbo@gmail.com>
2023-10-28 18:39:32 +0800

    feat: rbac casl validate fields

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -41,7 +41,7 @@ export default class UserController extends Controller {
     if (!isPwdCorrect) {
       return ctx.helper.error({ ctx, errorType: 'loginCheckFailInfo' });
     }
-    const token = sign({ username: user.username, _id: user._id }, app.config.jwt.secret, { expiresIn: '1h' });
+    const token = sign({ username: user.username, _id: user._id }, app.config.jwt.secret, { expiresIn: '48h' });
     ctx.helper.success({ ctx, res: { token }, msg: '登录成功' });
   }
 
--- app/decorator/checkPermission.ts
+++ app/decorator/checkPermission.ts
@@ -2,6 +2,8 @@ import { Controller } from 'egg';
 import { GlobalErrorTypes } from '../error';
 import defineRoles from '../roles/roles';
 import { subject } from '@casl/ability';
+import { permittedFieldsOf } from '@casl/ability/extra';
+import { difference } from 'lodash';
 
 const caslMethodMapping: Record<string, string> = {
   GET: 'read',
@@ -10,7 +12,11 @@ const caslMethodMapping: Record<string, string> = {
   DELETE: 'delete',
 };
 
-export default function checkPermission(modelName: string, errorType: GlobalErrorTypes, userKey = 'user') {
+const options = {
+  fieldsFrom: rule => rule.fields || [/* list of all fields for Article */],
+};
+
+export default function checkPermission(modelName: string, errorType: GlobalErrorTypes) {
   return function(prototype, key: string, descriptor: PropertyDescriptor) {
     const originMethod = descriptor.value;
     descriptor.value = async function(args) {
@@ -28,13 +34,23 @@ export default function checkPermission(modelName: string, errorType: GlobalErro
       const ability = defineRoles(ctx.state.user);
       const rule = ability.relevantRuleFor(action, modelName);
       let permission = false;
+      let keyPermission = true;
       if (rule && rule.conditions) {
         const certainRecord = await ctx.model[modelName].findOne({ id }).lean();
         permission = ability.can(action, subject(modelName, certainRecord));
       } else {
         permission = ability.can(action, modelName);
       }
-      if (!permission) {
+      // 判断 rule 中是否有受限字段
+      if (rule && rule.fields) {
+        const fields = permittedFieldsOf(ability, action, modelName, options);
+        if (fields.length > 0) {
+          const payloadKeys = Object.keys(ctx.request.body);
+          const diffKeys = difference(payloadKeys, fields);
+          keyPermission = diffKeys.length === 0;
+        }
+      }
+      if (!permission || !keyPermission) {
         return ctx.helper.error({ ctx, errorType });
       }
       await originMethod.apply(this, args);
